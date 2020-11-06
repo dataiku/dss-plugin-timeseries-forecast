d2785f931524b77cffa944154af42aa740a03e16
Alexandre Bourret <alexandre.bourret@dataiku.com>
2020-11-06 12:02:32 +0100

    SingleModel evaluate refacto

--- python-lib/dku_timeseries/single_model.py
+++ python-lib/dku_timeseries/single_model.py
@@ -52,10 +52,7 @@ class SingleModel():
     def evaluate(self, train_ds, test_ds, make_forecasts=False):
         # TODO split into multiple functions 
         logging.info("Timeseries forecast - Training model {} for evaluation".format(self.model_name))
-        if self.estimator is None:
-            predictor = self.model_descriptor.get_predictor(freq=self.frequency, prediction_length=self.prediction_length)
-        else:
-            predictor = self.estimator.train(train_ds)
+        predictor = self._get_predictor(train_ds)
         evaluator = Evaluator()
 
         forecast_it, ts_it = make_evaluation_predictions(
@@ -92,7 +89,23 @@ class SingleModel():
         item_metrics = item_metrics[
             [METRICS_DATASET.TARGET_COLUMN] + identifiers_columns + [METRICS_DATASET.MODEL_COLUMN] + EVALUATION_METRICS
         ]
-        item_metrics['model_params'] = str({
+        item_metrics['model_params'] = self._get_model_parameters_json()
+
+        if make_forecasts:
+            forecasts_df = self._get_forecasts_df(forecasts_list, train_ds)
+            return agg_metrics, item_metrics, forecasts_df, identifiers_columns
+
+        return agg_metrics, item_metrics
+
+    def _get_predictor(self, train_ds):
+        if self.estimator is None:
+            predictor = self.model_descriptor.get_predictor(freq=self.frequency, prediction_length=self.prediction_length)
+        else:
+            predictor = self.estimator.train(train_ds)
+        return predictor
+
+    def _get_model_parameters_json(self):
+        return str({
             'model_name': self.model_name,
             'model_parameters': self.model_parameters,
             'frequency': self.frequency,
@@ -102,32 +115,29 @@ class SingleModel():
             'batch_size': self.batch_size
         })
 
-        if make_forecasts:
-            all_timeseries = {}
-            for i, sample_forecasts in enumerate(forecasts_list):
-                series = sample_forecasts.mean_ts.rename("{}_{}".format(train_ds.list_data[i]['target_name'], self.model_name))
-                if 'identifiers' in train_ds.list_data[i]:
-                    timeseries_identifier_key = tuple(sorted(train_ds.list_data[i]['identifiers'].items()))
-                else:
-                    timeseries_identifier_key = None
+    def _get_forecasts_df(self, forecasts_list, train_ds):
+        all_timeseries = {}
+        for i, sample_forecasts in enumerate(forecasts_list):
+            series = sample_forecasts.mean_ts.rename("{}_{}".format(train_ds.list_data[i]['target_name'], self.model_name))
+            if 'identifiers' in train_ds.list_data[i]:
+                timeseries_identifier_key = tuple(sorted(train_ds.list_data[i]['identifiers'].items()))
+            else:
+                timeseries_identifier_key = None
 
-                if timeseries_identifier_key in all_timeseries:
-                    all_timeseries[timeseries_identifier_key] += [series]
-                else:
-                    all_timeseries[timeseries_identifier_key] = [series]
+            if timeseries_identifier_key in all_timeseries:
+                all_timeseries[timeseries_identifier_key] += [series]
+            else:
+                all_timeseries[timeseries_identifier_key] = [series]
 
-            multiple_df = []
-            for timeseries_identifier_key, series_list in all_timeseries.items():
-                unique_identifiers_df = pd.concat(series_list, axis=1).reset_index(drop=False)
-                if timeseries_identifier_key:
-                    for identifier_key, identifier_value in timeseries_identifier_key:
-                        unique_identifiers_df[identifier_key] = identifier_value
-                multiple_df += [unique_identifiers_df]
-            forecasts_df = pd.concat(multiple_df, axis=0).reset_index(drop=True)
-
-            return agg_metrics, item_metrics, forecasts_df, identifiers_columns
-
-        return agg_metrics, item_metrics
+        multiple_df = []
+        for timeseries_identifier_key, series_list in all_timeseries.items():
+            unique_identifiers_df = pd.concat(series_list, axis=1).reset_index(drop=False)
+            if timeseries_identifier_key:
+                for identifier_key, identifier_value in timeseries_identifier_key:
+                    unique_identifiers_df[identifier_key] = identifier_value
+            multiple_df += [unique_identifiers_df]
+        forecasts_df = pd.concat(multiple_df, axis=0).reset_index(drop=True)
+        return forecasts_df
 
     def save(self, model_folder, version_name):
         # TODO ? move outside of the class as it interacts with dataiku.Folder objects
