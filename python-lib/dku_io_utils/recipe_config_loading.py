4cd5a1bab451c02011c410e18c79a73e3f02ceb8
StanislasGuinel <stanislas.guinel@dataiku.com>
2021-01-22 18:38:13 +0100

    also for quick automl

--- python-lib/dku_io_utils/recipe_config_loading.py
+++ python-lib/dku_io_utils/recipe_config_loading.py
@@ -118,7 +118,7 @@ def load_training_config(recipe_config):
     # Overwrite values in case of autoML mode selected
     if params["forecasting_style"] == "auto":
         params["epoch"] = 10
-        params["batch_size"] = 32
+        params["batch_size"] = 256 if params["use_gpu"] else 32
         params["num_batches_per_epoch"] = 50
     elif params["forecasting_style"] == "auto_performance":
         params["context_length"] = params["prediction_length"]
