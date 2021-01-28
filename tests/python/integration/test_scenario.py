e5ba5c7e824b9151eb85cac5592313066f98a575
Alex Combessie <alex.combessie@gmail.com>
2021-01-28 15:38:25 +0100

    Update to latest integration test framework

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -1,34 +1,19 @@
 import pytest
-import logging
 
 from dku_plugin_test_utils import dss_scenario
 
 
-pytestmark = pytest.mark.usefixtures("plugin", "dss_target")
+def test_run_timeseries_forecast_regular(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key="TIMESERIESTEST", scenario_id="Regular")
 
 
-test_kwargs = {
-    "user": "user1",
-    "project_key": "TIMESERIESTEST",
-    "logger": logging.getLogger("dss-plugin-test.timeseries-forecast.test_scenario"),
-}
+def test_run_timeseries_forecast_partition(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key="TIMESERIESTEST", scenario_id="Partitions")
 
 
-def test_run_timeseries_forecast_regular(user_clients):
-    test_kwargs["client"] = user_clients[test_kwargs["user"]]
-    dss_scenario.run(scenario_id="Regular", **test_kwargs)
+def test_run_timeseries_sql_forecast_regular(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key="TIMESERIESTEST", scenario_id="SQLRegular")
 
 
-def test_run_timeseries_forecast_partition(user_clients):
-    test_kwargs["client"] = user_clients[test_kwargs["user"]]
-    dss_scenario.run(scenario_id="Partitions", **test_kwargs)
-
-
-def test_run_timeseries_sql_forecast_regular(user_clients):
-    test_kwargs["client"] = user_clients[test_kwargs["user"]]
-    dss_scenario.run(scenario_id="SQLRegular", **test_kwargs)
-
-
-def test_run_timeseries_sql_forecast_partition(user_clients):
-    test_kwargs["client"] = user_clients[test_kwargs["user"]]
-    dss_scenario.run(scenario_id="SQLPartitions", **test_kwargs)
+def test_run_timeseries_sql_forecast_partition(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key="TIMESERIESTEST", scenario_id="SQLPartitions")
