0d0731ca6dd677d7735d77f9bebf38729b5df0f6
Alex Combessie <alex.combessie@gmail.com>
2021-01-28 15:47:05 +0100

    Update test_scenario.py

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -1,19 +1,21 @@
-import pytest
-
+# -*- coding: utf-8 -*-
 from dku_plugin_test_utils import dss_scenario
 
 
+TEST_PROJECT_KEY = "TIMESERIESTEST"
+
+
 def test_run_timeseries_forecast_regular(user_dss_clients):
-    dss_scenario.run(user_dss_clients, project_key="TIMESERIESTEST", scenario_id="Regular")
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="Regular")
 
 
 def test_run_timeseries_forecast_partition(user_dss_clients):
-    dss_scenario.run(user_dss_clients, project_key="TIMESERIESTEST", scenario_id="Partitions")
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="Partitions")
 
 
 def test_run_timeseries_sql_forecast_regular(user_dss_clients):
-    dss_scenario.run(user_dss_clients, project_key="TIMESERIESTEST", scenario_id="SQLRegular")
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="SQLRegular")
 
 
 def test_run_timeseries_sql_forecast_partition(user_dss_clients):
-    dss_scenario.run(user_dss_clients, project_key="TIMESERIESTEST", scenario_id="SQLPartitions")
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="SQLPartitions")
