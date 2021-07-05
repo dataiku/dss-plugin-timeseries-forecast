e60fa3db6230fa50796b7f982a1f29045d27ea87
StanislasGuinel <stanislas.guinel@dataiku.com>
2021-07-05 21:50:53 +0200

    fix unit tests

--- tests/python/unit/test_model_handler.py
+++
@@ -1,6 +0,0 @@
-from gluonts_forecasts.model_handler import ModelHandler
-
-
-def test_get_model_descriptor():
-    model = ModelHandler("no model")
-    assert model._get_model_descriptor() is not None
