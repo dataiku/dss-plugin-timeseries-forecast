f827f670bd95febc342eaab4bd4a2094fad3a151
StanislasGuinel <stanislas.guinel@dataiku.com>
2021-07-05 21:50:34 +0200

    one model handler class per model

--- python-lib/gluonts_forecasts/model.py
+++ python-lib/gluonts_forecasts/model.py
@@ -1,8 +1,14 @@
-from dku_constants import EVALUATION_METRICS_DESCRIPTIONS, METRICS_DATASET, TIMESERIES_KEYS, CUSTOMISABLE_FREQUENCIES_OFFSETS
+from dku_constants import (
+    EVALUATION_METRICS_DESCRIPTIONS,
+    METRICS_DATASET,
+    TIMESERIES_KEYS,
+    CUSTOMISABLE_FREQUENCIES_OFFSETS,
+)
 from gluonts.evaluation.backtest import make_evaluation_predictions
 from gluonts.evaluation import Evaluator
 from gluonts_forecasts.gluon_dataset import remove_unused_external_features
-from gluonts_forecasts.model_handler import ModelHandler
+
+from gluonts_forecasts.model_handler import ModelHandlerRegistry
 from gluonts_forecasts.utils import concat_timeseries_per_identifiers, concat_all_timeseries, quantile_forecasts_series
 from time import perf_counter
 from pandas.tseries.frequencies import to_offset
@@ -26,7 +32,7 @@ class ModelPredictionError(Exception):
     pass
 
 
-class Model(ModelHandler):
+class Model:
     """
     Wrapper class to train and evaluate a GluonTS estimator, and retrieve the evaluation metrics and predictions
 
@@ -56,15 +62,21 @@ class Model(ModelHandler):
         season_length=None,
         mxnet_context=None,
     ):
-        super().__init__(model_name)
+        self.model_handler = ModelHandlerRegistry.get(model_name)
+
         self.model_name = model_name
         self.model_parameters = model_parameters
         self.custom_frequency = frequency
-        self.frequency = frequency if not isinstance(to_offset(frequency), CUSTOMISABLE_FREQUENCIES_OFFSETS) else to_offset(frequency).name.split("-")[0]
+        self.frequency = (
+            frequency
+            if not isinstance(to_offset(frequency), CUSTOMISABLE_FREQUENCIES_OFFSETS)
+            else to_offset(frequency).name.split("-")[0]
+        )
         self.prediction_length = prediction_length
         self.epoch = epoch
-        self.use_external_features = use_external_features and ModelHandler.can_use_external_feature(self)
-        self.use_seasonality = ModelHandler.can_use_seasonality(self)
+        # self.use_external_features = use_external_features and ModelHandler.can_use_external_feature(self)
+        self.use_external_features = use_external_features and self.model_handler.can_use_external_feature()
+        self.use_seasonality = self.model_handler.can_use_seasonality()
         self.mxnet_context = mxnet_context
 
         self.estimator_kwargs = {
@@ -78,7 +90,7 @@ class Model(ModelHandler):
         self.num_batches_per_epoch = num_batches_per_epoch
         if self.num_batches_per_epoch is not None:
             trainer_kwargs.update({"num_batches_per_epoch": self.num_batches_per_epoch})
-        self.trainer = ModelHandler.trainer(self, **trainer_kwargs)
+        self.trainer = self.model_handler.trainer(**trainer_kwargs)
         if self.trainer is not None:
             self.estimator_kwargs.update({"trainer": self.trainer})
         else:
@@ -88,7 +100,7 @@ class Model(ModelHandler):
             self.estimator_kwargs.update({"season_length": self.season_length})
         if self.use_external_features:
             self.estimator_kwargs.update({"use_feat_dynamic_real": True})
-        self.estimator = ModelHandler.estimator(self, self.model_parameters, **self.estimator_kwargs)
+        self.estimator = self.model_handler.estimator(self.model_parameters, **self.estimator_kwargs)
         self.predictor = None
         self.evaluation_time = 0
         self.retraining_time = 0
@@ -99,15 +111,17 @@ class Model(ModelHandler):
     def train(self, train_list_dataset, reinit=True):
         """Train model on train_list_dataset and re-instanciate estimator if reinit=True"""
         start = perf_counter()
-        logger.info(f"Re-training {self.get_label()} model on entire dataset ...")
+        logger.info(f"Re-training {self.model_handler.get_label()} model on entire dataset ...")
 
         if reinit:  # re-instanciate model to re-initialize model parameters
-            self.estimator = ModelHandler.estimator(self, self.model_parameters, **self.estimator_kwargs)
+            self.estimator = self.model_handler.estimator(self.model_parameters, **self.estimator_kwargs)
 
         self.predictor = self._train_estimator(train_list_dataset)
 
         self.retraining_time = perf_counter() - start
-        logger.info(f"Re-training {self.get_label()} model on entire dataset: Done in {self.retraining_time:.2f} seconds")
+        logger.info(
+            f"Re-training {self.model_handler.get_label()} model on entire dataset: Done in {self.retraining_time:.2f} seconds"
+        )
 
     def train_evaluate(self, train_list_dataset, test_list_dataset, make_forecasts=False, retrain=False):
         """Train Model on train_list_dataset and evaluate it on test_list_dataset. Then retrain on test_list_dataset if retrain=True.
@@ -127,13 +141,17 @@ class Model(ModelHandler):
             train_list_dataset = remove_unused_external_features(train_list_dataset, self.frequency)
             test_list_dataset = remove_unused_external_features(test_list_dataset, self.frequency)
 
-        logger.info(f"Evaluating {self.get_label()} model performance...")
+        logger.info(f"Evaluating {self.model_handler.get_label()} model performance...")
         start = perf_counter()
         evaluation_predictor = self._train_estimator(train_list_dataset)
 
-        agg_metrics, item_metrics, forecasts = self._make_evaluation_predictions(evaluation_predictor, test_list_dataset)
+        agg_metrics, item_metrics, forecasts = self._make_evaluation_predictions(
+            evaluation_predictor, test_list_dataset
+        )
         self.evaluation_time = perf_counter() - start
-        logger.info(f"Evaluating {self.get_label()} model performance: Done in {self.evaluation_time:.2f} seconds")
+        logger.info(
+            f"Evaluating {self.model_handler.get_label()} model performance: Done in {self.evaluation_time:.2f} seconds"
+        )
 
         if retrain:
             self.train(test_list_dataset)
@@ -161,10 +179,14 @@ class Model(ModelHandler):
             List of gluonts.model.forecast.Forecast (objects storing the predicted distributions as samples).
         """
         try:
-            forecast_it, ts_it = make_evaluation_predictions(dataset=test_list_dataset, predictor=predictor, num_samples=100)
+            forecast_it, ts_it = make_evaluation_predictions(
+                dataset=test_list_dataset, predictor=predictor, num_samples=100
+            )
             forecasts = list(forecast_it)
         except Exception as err:
-            raise ModelPredictionError(f"GluonTS '{self.model_name}' model crashed when making predictions. Full error: {err}")
+            raise ModelPredictionError(
+                f"GluonTS '{self.model_name}' model crashed when making predictions. Full error: {err}"
+            )
         evaluator = Evaluator(num_workers=min(2, multiprocessing.cpu_count()))
         agg_metrics, item_metrics = evaluator(ts_it, forecasts, num_series=len(test_list_dataset))
         return agg_metrics, item_metrics, forecasts
@@ -180,18 +202,22 @@ class Model(ModelHandler):
         Returns:
             DataFrame of metrics, model name, target column and identifiers columns.
         """
-        item_metrics[METRICS_DATASET.MODEL_COLUMN] = ModelHandler.get_label(self)
-        agg_metrics[METRICS_DATASET.MODEL_COLUMN] = ModelHandler.get_label(self)
+        item_metrics[METRICS_DATASET.MODEL_COLUMN] = self.model_handler.get_label()
+        agg_metrics[METRICS_DATASET.MODEL_COLUMN] = self.model_handler.get_label()
 
         identifiers_columns = (
-            list(train_list_dataset.list_data[0][TIMESERIES_KEYS.IDENTIFIERS].keys()) if TIMESERIES_KEYS.IDENTIFIERS in train_list_dataset.list_data[0] else []
+            list(train_list_dataset.list_data[0][TIMESERIES_KEYS.IDENTIFIERS].keys())
+            if TIMESERIES_KEYS.IDENTIFIERS in train_list_dataset.list_data[0]
+            else []
         )
         identifiers_values = {identifiers_column: [] for identifiers_column in identifiers_columns}
         target_columns = []
         for univariate_timeseries in train_list_dataset.list_data:
             target_columns += [univariate_timeseries[TIMESERIES_KEYS.TARGET_NAME]]
             for identifiers_column in identifiers_columns:
-                identifiers_values[identifiers_column] += [univariate_timeseries[TIMESERIES_KEYS.IDENTIFIERS][identifiers_column]]
+                identifiers_values[identifiers_column] += [
+                    univariate_timeseries[TIMESERIES_KEYS.IDENTIFIERS][identifiers_column]
+                ]
 
         item_metrics[METRICS_DATASET.TARGET_COLUMN] = target_columns
         agg_metrics[METRICS_DATASET.TARGET_COLUMN] = METRICS_DATASET.AGGREGATED_ROW
@@ -199,7 +225,9 @@ class Model(ModelHandler):
 
         for identifiers_column in identifiers_columns:
             item_metrics[identifiers_column] = identifiers_values[identifiers_column]
-            agg_metrics[identifiers_column] = METRICS_DATASET.AGGREGATED_ROW  # or keep empty but will cast integer to float
+            agg_metrics[
+                identifiers_column
+            ] = METRICS_DATASET.AGGREGATED_ROW  # or keep empty but will cast integer to float
 
         metrics = item_metrics.append(agg_metrics, ignore_index=True)
 
@@ -226,24 +254,26 @@ class Model(ModelHandler):
         """
         kwargs = {"freq": self.frequency, "prediction_length": self.prediction_length}
         if self.estimator is None:
-            if ModelHandler.needs_num_samples(self):
+            if self.model_handler.needs_num_samples():
                 kwargs.update({"num_samples": 100})
             if self.use_seasonality and self.season_length:
                 kwargs.update({"season_length": self.season_length})
-            predictor = ModelHandler.predictor(self, **kwargs)
+            predictor = self.model_handler.predictor(**kwargs)
         else:
             try:
                 predictor = self.estimator.train(train_list_dataset)
             except Exception as err:
-                raise ModelTrainingError(f"GluonTS '{self.model_name}' model crashed during training. Full error: {err}")
+                raise ModelTrainingError(
+                    f"GluonTS '{self.model_name}' model crashed during training. Full error: {err}"
+                )
         return predictor
 
     def _get_model_parameters_json(self, train_list_dataset):
-        """ Returns a JSON string containing model parameters and results """
+        """Returns a JSON string containing model parameters and results"""
         timeseries_number = len(train_list_dataset.list_data)
         timeseries_total_length = sum([len(ts[TIMESERIES_KEYS.TARGET]) for ts in train_list_dataset.list_data])
         model_params = {
-            "model_name": ModelHandler.get_label(self),
+            "model_name": self.model_handler.get_label(),
             "frequency": self.frequency,
             "prediction_length": self.prediction_length,
             "use_external_features": self.use_external_features,
@@ -277,7 +307,9 @@ class Model(ModelHandler):
                 f"{self.model_name}_{train_list_dataset.list_data[i][TIMESERIES_KEYS.TARGET_NAME]}"
             )
             if TIMESERIES_KEYS.IDENTIFIERS in train_list_dataset.list_data[i]:
-                timeseries_identifier_key = tuple(sorted(train_list_dataset.list_data[i][TIMESERIES_KEYS.IDENTIFIERS].items()))
+                timeseries_identifier_key = tuple(
+                    sorted(train_list_dataset.list_data[i][TIMESERIES_KEYS.IDENTIFIERS].items())
+                )
             else:
                 timeseries_identifier_key = None
 
--- python-lib/gluonts_forecasts/model_handler.py
+++ python-lib/gluonts_forecasts/model_handler.py
@@ -20,91 +20,13 @@ from gluonts.mx.distribution import StudentTOutput, GaussianOutput, NegativeBino
 from gluonts_forecasts.utils import sanitize_model_parameters
 
 
-ESTIMATOR = "estimator"
-CAN_USE_EXTERNAL_FEATURES = "can_use_external_feature"
-CAN_USE_SEASONALITY = "can_use_seasonality"
-DEFAULT_KWARGS = "default_kwargs"
-TRAINER = "trainer"
-PREDICTOR = "predictor"
-NEEDS_NUM_SAMPLES = "needs_num_samples"
-LABEL = "label"
-IS_NAIVE = "is_naive"
-
-
-MODEL_DESCRIPTORS = {
-    "trivial_identity": {
-        LABEL: "TrivialIdentity",
-        CAN_USE_EXTERNAL_FEATURES: False,
-        ESTIMATOR: None,
-        PREDICTOR: IdentityPredictor,
-        TRAINER: None,
-        NEEDS_NUM_SAMPLES: True,
-        IS_NAIVE: True,
-        DEFAULT_KWARGS: {"num_samples": 100},
-    },
-    "seasonal_naive": {
-        LABEL: "SeasonalNaive",
-        CAN_USE_EXTERNAL_FEATURES: False,
-        ESTIMATOR: None,
-        PREDICTOR: SeasonalNaivePredictor,
-        TRAINER: None,
-        IS_NAIVE: True,
-        CAN_USE_SEASONALITY: True,
-    },
-    "autoarima": {
-        LABEL: "AutoARIMA",
-        CAN_USE_EXTERNAL_FEATURES: True,
-        ESTIMATOR: AutoARIMAEstimator,
-        PREDICTOR: AutoARIMAPredictor,
-        TRAINER: None,
-        CAN_USE_SEASONALITY: True,
-    },
-    "seasonal_trend": {
-        LABEL: "SeasonalTrend",
-        CAN_USE_EXTERNAL_FEATURES: False,
-        ESTIMATOR: SeasonalTrendEstimator,
-        PREDICTOR: SeasonalTrendPredictor,
-        TRAINER: None,
-        CAN_USE_SEASONALITY: True,
-    },
-    "npts": {
-        LABEL: "NPTS",
-        CAN_USE_EXTERNAL_FEATURES: False,
-        ESTIMATOR: None,
-        PREDICTOR: NPTSPredictor,
-        TRAINER: None,
-        IS_NAIVE: True,
-    },
-    "simplefeedforward": {
-        LABEL: "FeedForward",
-        CAN_USE_EXTERNAL_FEATURES: False,
-        ESTIMATOR: SimpleFeedForwardEstimator,
-        TRAINER: Trainer,
-    },
-    "deepar": {
-        LABEL: "DeepAR",
-        CAN_USE_EXTERNAL_FEATURES: True,
-        ESTIMATOR: DeepAREstimator,
-        TRAINER: Trainer,
-    },
-    "transformer": {
-        LABEL: "Transformer",
-        CAN_USE_EXTERNAL_FEATURES: True,
-        ESTIMATOR: TransformerEstimator,
-        TRAINER: Trainer,
-    },
-    "mqcnn": {
-        LABEL: "MQ-CNN",
-        CAN_USE_EXTERNAL_FEATURES: True,
-        ESTIMATOR: MQCNNEstimator,
-        TRAINER: Trainer,
-    },
-}
-
-
 # these parameter are classes but are set as strings in the UI
 CLASS_PARAMETERS = {
-    "distr_output": {"StudentTOutput()": StudentTOutput(), "GaussianOutput()": GaussianOutput(), "NegativeBinomialOutput()": NegativeBinomialOutput()},
+    "distr_output": {
+        "StudentTOutput()": StudentTOutput(),
+        "GaussianOutput()": GaussianOutput(),
+        "NegativeBinomialOutput()": NegativeBinomialOutput(),
+    },
     "model": {"ARIMA": ARIMA, "ETSModel": ETSModel},
 }
 
@@ -118,65 +40,55 @@ class ModelParameterError(ValueError):
 class ModelHandler:
     """
     Class to retrieve the estimator, trainer or descriptor of a GluonTS model
-
-    Attributes:
-        model_name (str)
     """
 
-    def __init__(self, model_name):
-        self.model_name = model_name
-        self.model_descriptor = self._get_model_descriptor()
-
-    def _get_model_descriptor(self):
-        model_descriptor = MODEL_DESCRIPTORS.get(self.model_name)
-        if model_descriptor is None:
-            return {}
-        else:
-            return model_descriptor
+    def __init__(self):
+        self.estimator_class = None
+        self.predictor_class = None
+        self.trainer_class = None
 
     def estimator(self, model_parameters, **kwargs):
-        default_kwargs = self.model_descriptor.get(DEFAULT_KWARGS, {})
-        kwargs.update(default_kwargs)
         model_parameters_sanitized = sanitize_model_parameters(model_parameters.get("kwargs", {}), self.model_name)
         kwargs.update(model_parameters_sanitized)
-        estimator = self.model_descriptor.get(ESTIMATOR)
         kwargs = self._convert_parameters_to_class(kwargs)
         try:
-            ret = None if estimator is None else estimator(**kwargs)
+            ret = None if self.estimator_class is None else self.estimator_class(**kwargs)
         except Exception as err:
-            raise ModelParameterError(f"Issue with parameters '{kwargs}' of model '{self.model_name}'. Full error: {err}")
+            raise ModelParameterError(f"Issue with parameters '{kwargs}' of model '{self.label}'. Full error: {err}")
         return ret
 
     def trainer(self, **kwargs):
-        trainer = self.model_descriptor.get(TRAINER)
         try:
-            ret = None if trainer is None else trainer(**kwargs)
+            ret = None if self.trainer_class is None else self.trainer_class(**kwargs)
         except Exception as err:
-            raise ModelParameterError(f"Issue with parameters '{kwargs}' of model trainer for '{self.model_name}'. Full error: {err}")
+            raise ModelParameterError(
+                f"Issue with parameters '{kwargs}' of model trainer for '{self.label}'. Full error: {err}"
+            )
         return ret
 
     def predictor(self, **kwargs):
-        predictor = self.model_descriptor.get(PREDICTOR)
         try:
-            ret = None if predictor is None else predictor(**kwargs)
+            ret = None if self.predictor_class is None else self.predictor_class(**kwargs)
         except Exception as err:
-            raise ModelParameterError(f"Issue with parameters '{kwargs}' of model predictor for {self.model_name}. Full error: {err}")
+            raise ModelParameterError(
+                f"Issue with parameters '{kwargs}' of model predictor for {self.label}. Full error: {err}"
+            )
         return ret
 
     def can_use_external_feature(self):
-        return self.model_descriptor.get(CAN_USE_EXTERNAL_FEATURES, False)
+        return False
 
     def can_use_seasonality(self):
-        return self.model_descriptor.get(CAN_USE_SEASONALITY, False)
+        return False
 
     def needs_num_samples(self):
-        return self.model_descriptor.get(NEEDS_NUM_SAMPLES, False)
+        return False
 
     def get_label(self):
-        return self.model_descriptor.get(LABEL, "")
+        return self.label
 
     def _convert_parameters_to_class(self, parameters):
-        """Evaluate string parameters that are classes so that they become instances of their class """
+        """Evaluate string parameters that are classes so that they become instances of their class"""
         parameters_converted = parameters.copy()
         for class_parameter, class_parameter_values in CLASS_PARAMETERS.items():
             if class_parameter in parameters_converted:
@@ -188,8 +100,130 @@ class ModelHandler:
                     """
                     )
                 else:
-                    parameters_converted[class_parameter] = class_parameter_values[parameters_converted[class_parameter]]
+                    parameters_converted[class_parameter] = class_parameter_values[
+                        parameters_converted[class_parameter]
+                    ]
         return parameters_converted
+
+
+class TrivialIdentity(ModelHandler):
+    def __init__(self):
+        super().__init__()
+        self.model_name = "trivial_identity"
+        self.label = "TrivialIdentity"
+        self.predictor_class = IdentityPredictor
+
+    def needs_num_samples(self):
+        return True
+
+    def estimator(self, model_parameters, **kwargs):
+        default_kwargs = {"num_samples": 100}
+        kwargs.update(default_kwargs)
+        return super().estimator(model_parameters, **kwargs)
+
+
+class SeasonalNaive(ModelHandler):
+    def __init__(self):
+        super().__init__()
+        self.model_name = "seasonal_naive"
+        self.label = "SeasonalNaive"
+        self.predictor_class = SeasonalNaivePredictor
+
+    def can_use_seasonality(self):
+        return True
+
+
+class AutoARIMA(ModelHandler):
+    def __init__(self):
+        super().__init__()
+        self.model_name = "autoarima"
+        self.label = "AutoARIMA"
+        self.estimator_class = AutoARIMAEstimator
+        self.predictor_class = AutoARIMAPredictor
+
+    def can_use_seasonality(self):
+        return True
+
+    def can_use_external_feature(self):
+        return True
+
+
+class SeasonalTrend(ModelHandler):
+    def __init__(self):
+        super().__init__()
+        self.model_name = "seasonal_trend"
+        self.label = "SeasonalTrend"
+        self.estimator_class = SeasonalTrendEstimator
+        self.predictor_class = SeasonalTrendPredictor
+
+    def can_use_seasonality(self):
+        return True
+
+
+class NPTS(ModelHandler):
+    def __init__(self):
+        super().__init__()
+        self.model_name = "npts"
+        self.label = "NPTS"
+        self.predictor_class = NPTSPredictor
+
+
+class FeedForward(ModelHandler):
+    def __init__(self):
+        super().__init__()
+        self.model_name = "simplefeedforward"
+        self.label = "FeedForward"
+        self.estimator_class = SimpleFeedForwardEstimator
+        self.trainer_class = Trainer
+
+
+class DeepAR(ModelHandler):
+    def __init__(self):
+        super().__init__()
+        self.model_name = "deepar"
+        self.label = "DeepAR"
+        self.estimator_class = DeepAREstimator
+        self.trainer_class = Trainer
+
+    def can_use_external_feature(self):
+        return True
+
+
+class Transformer(ModelHandler):
+    def __init__(self):
+        super().__init__()
+        self.model_name = "transformer"
+        self.label = "Transformer"
+        self.estimator_class = TransformerEstimator
+        self.trainer_class = Trainer
+
+    def can_use_external_feature(self):
+        return True
+
+
+class MQCNN(ModelHandler):
+    def __init__(self):
+        super().__init__()
+        self.model_name = "mqcnn"
+        self.label = "MQ-CNN"
+        self.estimator_class = MQCNNEstimator
+        self.trainer_class = Trainer
+
+    def can_use_external_feature(self):
+        return True
+
+
+ModelHandlerRegistry = {
+    "trivial_identity": TrivialIdentity(),
+    "seasonal_naive": SeasonalNaive(),
+    "autoarima": AutoARIMA(),
+    "seasonal_trend": SeasonalTrend(),
+    "npts": NPTS(),
+    "simplefeedforward": FeedForward(),
+    "deepar": DeepAR(),
+    "transformer": Transformer(),
+    "mqcnn": MQCNN(),
+}
 
 
 def list_available_models():
@@ -198,7 +232,7 @@ def list_available_models():
     Returns:
         dict_keys of model names.
     """
-    available_models = MODEL_DESCRIPTORS.copy()
+    available_models = ModelHandlerRegistry.copy()
     return available_models.keys()
 
 
@@ -208,23 +242,25 @@ def list_available_models_labels():
     Returns:
         List of model names.
     """
-    available_models = MODEL_DESCRIPTORS.copy()
+    available_models = ModelHandlerRegistry.copy()
     available_models_labels = []
     for model in available_models:
-        label = available_models[model].get(LABEL)
+        label = available_models[model].get_label()
         if label is not None:
             available_models_labels.append(label)
     return available_models_labels
 
 
 def get_model_label(model_name):
-    model_descriptor = MODEL_DESCRIPTORS.get(model_name)
-    if model_descriptor is None:
+    model_handler = ModelHandlerRegistry.get(model_name)
+    if model_handler is None:
         return None
     else:
-        return model_descriptor.get(LABEL, "")
+        return model_handler.get_label()
 
 
 def get_model_name_from_label(model_label):
-    available_models = MODEL_DESCRIPTORS.copy()
-    return next((model_name for model_name in available_models if available_models[model_name].get(LABEL) == model_label), None)
+    available_models = ModelHandlerRegistry.copy()
+    return next(
+        (model_name for model_name in available_models if available_models[model_name].get_label() == model_label), None
+    )
